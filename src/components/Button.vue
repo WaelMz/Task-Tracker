acd21fcc295182ca5cb704b008950ec69f2d0470
Wael Mahmoud <waelmahmoude@gmail.com>
2021-11-09 14:26:51 +0200

    finish project

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button :style="{background: color}" class="btn" v-on:click="$emit('btn-click')">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name:'Button',
+    props:{
+        text:String,
+        color:String
+    }
+}
+</script>
---
+++ src/components/Task.vue
@@ -0,0 +1,38 @@
+<template>
+    <div @dblclick="$emit('toggle-reminder',task.id)" :class="[task.reminder ? 'reminder' : '','tasks']">
+        <h3>{{ task.text }}
+            <i @click="$emit('delete-task',task.id)" class="fas fa-times"></i>
+        </h3>
+        <p>{{ task.day }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Task',
+    props:{
+        task:Object
+    },
+}
+</script>
+
+<style scoped>
+    .fas{
+        color: red;
+    }
+    .tasks{
+        background: #f4f4f4;
+        margin: 5px;
+        padding: 10px 20px;
+        cursor:pointer;
+    }
+    .tasks.reminder{
+        border-left: 5px solid green;
+    }
+
+    .tasks.h3{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
